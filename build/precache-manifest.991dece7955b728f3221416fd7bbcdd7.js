46539d64262bdebbd6b6b3f7e6975ceb21390e99
Daniel Miller <danielemiller@live.com>
2021-01-27 14:33:52 -0500

    Fix deployment scripts

---
+++ build/precache-manifest.991dece7955b728f3221416fd7bbcdd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1669234ca6db0d293a9aefc1f8a6fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03dc680c89a84b6c6c6e",
+    "url": "./static/css/main.f37ddea3.chunk.css"
+  },
+  {
+    "revision": "996f2eb349037e44f98e",
+    "url": "./static/js/2.f5403096.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.f5403096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dc680c89a84b6c6c6e",
+    "url": "./static/js/main.1f729201.chunk.js"
+  },
+  {
+    "revision": "742e9357be680aebb716",
+    "url": "./static/js/runtime-main.a342f7d4.js"
+  },
+  {
+    "revision": "1a20727f8d794d3cb64077659e90f16a",
+    "url": "./static/media/Group 3.1a20727f.svg"
+  },
+  {
+    "revision": "2a3043cf5069c142af4257fe371d20e3",
+    "url": "./static/media/hero.2a3043cf.svg"
+  }
+]);
